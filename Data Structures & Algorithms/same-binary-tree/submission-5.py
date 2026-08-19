e2444c20c2e9595f19383e29cefb33b04f0ce896
Praagnya Parimi <PES2UG23CS417@pesu.pes.edu>
2026-08-20 00:47:44 +0530

    Add: same-binary-tree - submission-5

---
+++ Data Structures & Algorithms/same-binary-tree/submission-5.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Sol 1
+        # p_inorder = []
+        # q_inorder = []
+        
+        # def inorder(node, res):
+        #     if node is None:
+        #         res.append(None)
+        #         return
+        #     inorder(node.left, res)
+        #     res.append(node.val)
+        #     inorder(node.right, res)
+        #     return res
+        
+        # p_inorder = inorder(p, [])
+        # q_inorder = inorder(q, [])
+
+        # print(p_inorder)
+        # print(q_inorder)
+
+        # if p_inorder == q_inorder:
+        #     return True
+        # return False
+            
+        def dfs(node1, node2):
+            if not node1 and not node2:
+                return True
+            
+            if (node1 and not node2) or (node2 and not node1):
+                return False
+            
+            if node1.val != node2.val:
+                return False
+            
+            if dfs(node1.left, node2.left) == False:
+                return False
+
+            if dfs(node1.right, node2.right) == False:
+                return False
+            
+            return True
+        
+        if dfs(p, q) == False:
+            return False
+        else:
+            return True
