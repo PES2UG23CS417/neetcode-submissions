6949a5ef75f1ca906ac9f3baced7d0956f5f4972
Praagnya Parimi <PES2UG23CS417@pesu.pes.edu>
2026-09-20 14:50:26 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev2 = 1 # dp[i-2]
+        prev1 = 1 # dp[i-1]
+
+        for i in range(2, n+1):
+            current = prev1 + prev2
+
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
